6f8aeae2a09ca8373cb3b1ea37a22fbc6f4b23b1
José Augusto Fachin dos Santos <joseafachin@gmail.com>
2023-09-13 22:13:38 -0300

    First
    First

---
+++ PD24S - Azimute/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PD24S___Azimute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
